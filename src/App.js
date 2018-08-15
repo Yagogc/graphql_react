02d808811f2977b4c8043b64d01fe2c54e00f7a2
Yago Gonzalez <jagogc@gmail.com>
2018-08-15 17:38:13 +0100

    Add ESlint and Prettier

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 import { ApolloProvider } from "react-apollo";
 import ApolloClient from "apollo-boost";
+import logo from "./logo.svg";
 
 const client = new ApolloClient({
   uri: "https://api-euwest.graphcms.com/v1/cjkvc7hft02ir01bnbhofcxxi/master"
 });
 
-class App extends Component {
-  render() {
-    return (
-      <ApolloProvider client={client}>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-        </div>
-      </ApolloProvider>
-    );
-  }
-}
+const App = () => (
+  <ApolloProvider client={client}>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <p className="App-intro">
+        To get started, edit
+        <code>src/App.js</code>
+        and save to reload.
+      </p>
+    </div>
+  </ApolloProvider>
+);
 
 export default App;
