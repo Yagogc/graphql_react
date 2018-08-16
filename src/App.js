4d701d925ab79f3dcbc225655a0af4eb7b94b49e
Yago Gonzalez <jagogc@gmail.com>
2018-08-16 14:11:12 +0100

    Add Query

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from "react";
 import "./App.css";
-import { ApolloProvider } from "react-apollo";
+import { ApolloProvider, Query } from "react-apollo";
 import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
 import logo from "./logo.svg";
 
 const client = new ApolloClient({
   uri: "https://api-euwest.graphcms.com/v1/cjkvc7hft02ir01bnbhofcxxi/master"
 });
+
+const POSTS_QUERY = gql`
+  {
+    posts {
+      id
+      title
+      body
+      createdAt
+    }
+  }
+`;
+
+// client.query({ POSTS_QUERY }).then(res => console.log(res.data.posts));
 
 const App = () => (
   <ApolloProvider client={client}>
@@ -15,11 +29,13 @@ const App = () => (
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="App-title">Welcome to React</h1>
       </header>
-      <p className="App-intro">
-        To get started, edit
-        <code>src/App.js</code>
-        and save to reload.
-      </p>
+      <Query query={POSTS_QUERY}>
+        {({ loading, data }) => {
+          if (loading) return "Loading...";
+          const { posts } = data;
+          return posts.map(post => <h1 key={post.id}>{post.title}</h1>);
+        }}
+      </Query>
     </div>
   </ApolloProvider>
 );
